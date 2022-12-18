264c73a729bd5b0b52ba474be0fe6c8bfbf9610e
Vladislav <vladi5lav.ilin@yandex.com>
2022-12-18 16:19:20 +0300

    first laba

---
+++ 2.2.py
@@ -0,0 +1,5 @@
+message = "Hello world"  # Создаём переменную с типом данных "Строка"
+print(message)  # Выводим сообщение
+
+message = "Hello this world"  # Заменяем собщение в нашей переменной
+print(message)  # Выводим сообщение
---
+++ 2.3.py
@@ -0,0 +1,3 @@
+F_name = "Neighbor"  # Создаём переменную с именем
+L_name = "Very Bad"  # Создаём переменную с вторым именем
+print("Hello, " + L_name + " " + F_name + ", stop drilling the walls!")  # Выводим сообщение с помощью компитенации
